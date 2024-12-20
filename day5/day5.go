92c3916d3369c83437d68f7051fa2968ccccccf2
InfernOwl <cchampagnie.1991@gmail.com>
2024-12-20 11:54:47 -0500

    Adding solve for day 5 part 2

--- day5/day5.go
+++ day5/day5.go
@@ -17,8 +17,6 @@ func check(e error) {
 }
 
 func main() {
-	fmt.Println("Hello World")
-
 	//Open the input file
 	data, err := os.Open("../inputs/day5.txt")
 	check(err)
@@ -42,7 +40,11 @@ func main() {
 			if strings.Contains(line, "|") {
 				addRule(rules, line)
 			} else {
-				total += updateCheck(rules, line)
+				//Part One
+				total += partOne(rules, line)
+
+				//Part Two
+				// total += partTwo(rules, line, false)
 			}
 		}
 	}
@@ -67,7 +69,7 @@ func addRule(rules map[string][]string, line string) {
 	}
 }
 
-func updateCheck(rules map[string][]string, update string) int {
+func partOne(rules map[string][]string, update string) int {
 	//Take the update and split into an array of page numbers
 	//For each rule, find the first location of the pivot page in the update
 	//If not first entry, join every page before pivot page into one string
@@ -98,6 +100,51 @@ func updateCheck(rules map[string][]string, update string) int {
 
 }
 
-func partTwo() {
+func partTwo(rules map[string][]string, update string, isFailure bool) int {
+	//Take the update and split into an array of page numbers
+	//For each rule, find the first location of the pivot page in the update
+	//If not first entry, join every page before pivot page into one string
+	//Join rule parameters split by '|' and use to create Regex to check against
+	updArr := strings.Split(update, ",")
+
+	for pivot, pages := range rules {
+		index := slices.Index(updArr, pivot)
+		if index != -1 && index != 0 {
+			updString := strings.Join(updArr[:index], ",")
+			pagesString := strings.Join(pages, "|")
+			ruleRegex := regexp.MustCompile(pagesString)
+
+			ruleCheck := ruleRegex.FindAllString(updString, -1)
+
+			//If it fails the rule check
+			//Remove fail numbers from beginning of string
+			//Add those numbers (ruleCheck result) after pivot value
+			//Append rest of array
+			//Recursively run partTwo to check if new update is valid
+			if ruleCheck != nil {
+				beginning := strings.Split(updString, ",")
+
+				for _, num := range ruleCheck {
+					numIndex := slices.Index(beginning, num)
+					if numIndex != -1 {
+						beginning = append(beginning[:numIndex], beginning[numIndex+1:]...)
+					}
+				}
+				newUpdArr := append(append(append(beginning, updArr[index]), ruleCheck...), updArr[index+1:]...)
+				newUpdate := strings.Join(newUpdArr, ",")
+				return partTwo(rules, newUpdate, true)
+			}
+		}
+	}
+
+	// If rule check does not fail. Return the center page ONLY if it had failed before
+	answer := 0
+	if isFailure {
+		answer, _ = strconv.Atoi(updArr[len(updArr)/2])
+	} else {
+		answer = 0
+	}
+
+	return answer
 
 }
